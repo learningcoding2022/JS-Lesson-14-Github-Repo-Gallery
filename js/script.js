f7aa9085a972961f6a9ffeac0bccf9873e0f2819
molly_b <brownmolly109@gmail.com>
2023-07-09 19:54:24 -0400

    display repo info final

--- js/script.js
+++ js/script.js
@@ -13,6 +13,14 @@ const selectsRepos = document.querySelector(".repos");
 //variable selects the sections with a class of "repo-data" where the individual repo data will appear
 const selectsRepoData = document.querySelector(".repo-data");
 
+//variable selects the Back to Repo Gallery Button
+const backToReposButton = document.querySelector(".view-repos");
+//why doesn't this need "view-repos hide"?
+
+//variable selects the input with the "search by name" placeholder
+const filterInput = document.querySelector("filter-repos");
+//why doesn't this need "filter-repos hide"?
+
 //fetch API JSON Data
 
 //create and name an async function to fetch information from your GitHub profile
@@ -52,9 +60,9 @@ const showUserData = function (data) {
 const gitRepos = async function () {
     const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
     //how do you get the endpoints used above??
-    const repoData = await fetchRepos.json();
+    const selectsRepoData = await fetchRepos.json();
     //which const do I logout to make sure the code is retrieving the repos?
-    displayRepoInfo(repoData);
+    displayRepoInfo(selectsRepoData);
 };
 
 //display info about your repos
@@ -107,9 +115,9 @@ const getRepoInfo = async function (repoName) {
 
 const displaySpecificRepoInfo = function (repoInfo, languages) {
     //empty the HTML of the section with a class of "repo-data"
-    repoData.innerHTML = "";
+    selectsRepoData.innerHTML = "";
     //Unhide (show) the “repo-data” element. 
-    repoData.classList.remove("hide");
+    selectsRepoData.classList.remove("hide");
     //Hide the element with the class of “repos”.
     selectsRepos.classList.add("hide");
     //create a new div element and add the selected repository's name, description, default branch, and link to its code on GitHub
@@ -121,9 +129,19 @@ const displaySpecificRepoInfo = function (repoInfo, languages) {
         <p>Languages: ${languages.join(", ")}</p>
         <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
     `;
-    repoData.append(div);
+    selectsRepoData.append(div);
 };
 
+//add a click event to the back button
+backToReposButton.addEventListener("click", function () {
+    //unhide (display) the section where all the repo information appears
+    repos.classList.remove("hide");
+    //add the "hide" class to the section where the undividual repo data will appear 
+    selectsRepoData.classList.add("hide");
+    //add the "hide" class to the Back to Repo Gallery button itself
+
+}
+);
 
 
 
