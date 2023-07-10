dbb2765ab40f8d21c7a0d20beb43c762c7e948c6
molly_b <brownmolly109@gmail.com>
2023-07-10 09:13:20 -0400

    create a dynamic search

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const username = "learningcoding2022";
 const repoList = document.querySelector(".repo-list");
 
 //variable selects the section with a class of "repos" where all your repo information appears
-const selectsRepos = document.querySelector(".repos");
+const allReposContainer = document.querySelector(".repos");
 
 //variable selects the sections with a class of "repo-data" where the individual repo data will appear
 const selectsRepoData = document.querySelector(".repo-data");
@@ -18,7 +18,7 @@ const backToReposButton = document.querySelector(".view-repos");
 //why doesn't this need "view-repos hide"?
 
 //variable selects the input with the "search by name" placeholder
-const filterInput = document.querySelector("filter-repos");
+const filterInput = document.querySelector(".filter-repos");
 //why doesn't this need "filter-repos hide"?
 
 //fetch API JSON Data
@@ -53,11 +53,11 @@ const showUserData = function (data) {
     // append the div to the overview element
     overview.append(div);
     //use json file to grab specific data/properties
-    gitRepos();
+    gitRepos(username);
 };
 
 //create and anem a new asynch function to fetch your repos
-const gitRepos = async function () {
+const gitRepos = async function (username) {
     const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
     //how do you get the endpoints used above??
     const selectsRepoData = await fetchRepos.json();
@@ -67,6 +67,8 @@ const gitRepos = async function () {
 
 //display info about your repos
 const displayRepoInfo = function (repos) {
+    //at the top of the function that displays your repos show the filterInput element
+    filterInput.classList.remove("hide");
     //where does the const repos come from?
     //inside the function, loop and create a list item for each repo and give each item: a class, h3
     for (const repo of repos) {
@@ -88,19 +90,18 @@ repoList.addEventListener("click", function (e) {
 }
 );
 
-
 //create a function to get specific repo info
 const getRepoInfo = async function (repoName) {
     //In the function, make a fetch request to grab information about the specific repository.
     const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
     const repoInfo = await fetchInfo.json();
-    console.log(repoInfo);
+    //console.log(repoInfo);
     //create an array of languages 
     //create a variable called fetchLanguagues to fetch data from language_url property of your repoInfo
     const fetchLanguages = await fetch(repoInfo.languages_url);
     //create a variable called languageData to save the JSON response
     const languageData = await fetchLanguages.json();
-    console.log(languageData);
+    //console.log(languageData);
     //when tested this pulled up all properties rather than just the languages
 
     //add each language to an empty array called languages- make a list of languages
@@ -114,12 +115,14 @@ const getRepoInfo = async function (repoName) {
 };
 
 const displaySpecificRepoInfo = function (repoInfo, languages) {
+    //shows the Back to Repo Gallery Button and the back to Repo Gallery button
+    backToReposButton.classList.remove("hide");
     //empty the HTML of the section with a class of "repo-data"
     selectsRepoData.innerHTML = "";
     //Unhide (show) the “repo-data” element. 
     selectsRepoData.classList.remove("hide");
     //Hide the element with the class of “repos”.
-    selectsRepos.classList.add("hide");
+    allReposContainer.classList.add("hide");
     //create a new div element and add the selected repository's name, description, default branch, and link to its code on GitHub
     const div = document.createElement("div");
     div.innerHTML = `
@@ -135,13 +138,34 @@ const displaySpecificRepoInfo = function (repoInfo, languages) {
 //add a click event to the back button
 backToReposButton.addEventListener("click", function () {
     //unhide (display) the section where all the repo information appears
-    repos.classList.remove("hide");
+    allReposContainer.classList.remove("hide");
     //add the "hide" class to the section where the undividual repo data will appear 
     selectsRepoData.classList.add("hide");
     //add the "hide" class to the Back to Repo Gallery button itself
+    backToReposButton.classList.add("hide");
+});
 
-}
-);
+// dynamic search
 
+// attach an "input" event listener to filterInput. Pass the event (e) the call back function
+filterInput.addEventListener("input", function (e) {
+    //add a const to capture the value of the search text
+    const searchText = e.target.value;
+    //create a variable called repos to select ALL elements on the page with a class of "repo"
+    const repos = document.querySelectorAll("repo");
+    //create a variable and assign it to the lowercase value of the search text.
+    const searchLowerText = searchText.toLowerCase();
 
+    //loop through each repo inside your repos element
+    for (const repo of repos) {
+        //inside the loop, create a variable and assign it to the lowercase value of the innerText of each repo
+        const repoLowerText = repo.innerText.toLowerCase();
+        if (repoLowerText.includes) {
+        //if the repo contains text, show it. If it doesn't contain the text, hide the repo.
+        repo.classList.remove("hide");
+        } else {
+            repo.classList.add("hide");
+        }
+    }
+});
 
